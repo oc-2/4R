2cf832626b309e83278388a2815dec0b3518fff4
oc-2 <185859974+oc-2@users.noreply.github.com>
2025-01-04 01:37:58 +0000

    multiple bug fixes

---
+++ CLI/app.c
@@ -0,0 +1,116 @@
+// https://github.com/athede-v/4R
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Function to clear the console screen
+void clearScreen() {
+    system("clear");
+}
+
+// Function to set the 8-char password
+void setPassword(char *password) {
+    char input[10];
+    while (1) {
+        printf("$4R >> Enter Your 8-Char Password (Type 'exit' for Cancel): ");
+        scanf("%s", input);
+
+        if (strcmp(input, "exit") == 0) {
+            printf("$4R >> Password not set.\n");
+            break;
+        } else if (strlen(input) != 8) {
+            printf("$4R >> WRONG! Password must be 8 characters long.\n");
+        } else {
+            strcpy(password, input);
+            printf("$4R >> Password Set.\n");
+            break;
+        }
+    }
+}
+
+// Function to run the 4R Sign In Way
+void runSignIn(const char *password) {
+    int isIn = 0;
+    if (password[0] == '\0') {
+        printf("$4R >> Please Set Password First (Type 'set pass').\n");
+        return;
+    }
+
+    while (!isIn) {
+        int randomIndexes[4];
+        for (int i = 0; i < 4; i++) {
+            randomIndexes[i] = rand() % 8 + 1;
+        }
+
+        clearScreen();
+        printf("\n\n    X-X-X-X\n");
+        printf("    %d-%d-%d-%d\n\n", randomIndexes[0], randomIndexes[1], randomIndexes[2], randomIndexes[3]);
+
+        char passwordCom[9];
+        for (int i = 0; i < 4; i++) {
+            passwordCom[i] = password[randomIndexes[i] - 1];
+        }
+        passwordCom[4] = '\0';
+
+        char tempCom[9];
+        for (int i = 0; i < 4; i++) {
+            printf("    X[%d] : ", randomIndexes[i]);
+            scanf("%s", tempCom + i);
+        }
+
+        if (strcmp(tempCom, passwordCom) == 0) {
+            printf("\n$4R >> Good Job, You're In!\n");
+            isIn = 1;
+        } else {
+            printf("\n$4R >> WRONG! Try Again.\n");
+        }
+    }
+}
+
+int main() {
+    srand(time(NULL));
+
+    char password[9] = "";
+    int isExit = 0;
+
+    while (!isExit) {
+        clearScreen();
+        printf("$UR >>> ");
+        char input[10];
+        scanf("%s", input);
+
+        if (strcmp(input, "exit") == 0) {
+            isExit = 1;
+        } else if (strcmp(input, "set") == 0) {
+            setPassword(password);
+        } else if (strcmp(input, "run") == 0) {
+            runSignIn(password);
+        } else {
+            printf("$4R >> Unknown Command\n");
+        }
+    }
+
+    printf("Goodbye!\n");
+    return 0;
+}
+
+//    Hello And Welcome To 4R Sing In Way :) 
+//    This The Way How it Works 
+//    You Can Be More Creative (I Mean You Can Change Code And Add It Some Protect Protocol To It And Make A Strong And Very Safe 4R)
+   
+//      X-X-X-X
+//      1-5-8-1
+   
+//    This Is Template Of The CLI So At First Enter Your 8-Char Password By 
+//     `$UR >>> set pass`
+//    Command , Type In The Console And Press Enter
+   
+//    And Then You Can Test 4R Sign In Way BY
+//     `$UR >>> run`
+//    Command , Just Type In The Console And Press Enter Bro 
+   
+//    -Some Options Are Active At The Moment For More Security (You Can Remove And Replace Your Code Or Change Any Thing You Want , It Just A Very Simple Code)
+   
+//    Type `exit` To Exit 
+//    So Have Fun :) (@athede-v)
--- CLI/app.py
+++ CLI/app.py
@@ -1,71 +1,68 @@
+#https://github.com/athede-v/4R
+
 import os
 from random import randint
 
-class FourRSignIn:
-    def __init__(self):
-        self.password = ''
-        self.is_in = False
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
 
-    def clear_screen(self):
-        if os.name == 'nt':
-            _ = os.system('cls')
-        else:
-            _ = os.system('clear')
+clear()
+print("Hello And Welcome To 4R Sing In Way :)\nThis The Way How it Works \nYou Can Be More Creative (I Mean You Can Change Code And Add It Some Protect Protocol To It And Make A Strong And Very Safe 4R)\n\n  X-X-X-X\n  1-5-8-1\n\nThis Is Template Of The CLI So At First Enter Your 8-Char Password By \n'$UR >>> set pass'\nCommand , Type In The Console And Press Enter\n\nAnd Then You Can Test 4R Sign In Way BY\n'$UR >>> run'\nCommand , Just Type In The Console And Press Enter Bro \n\n-Some Options Are Active At The Moment For More Security (You Can Remove And Replace Your Code Or Change Any Thing You Want , It Just A Very Simple Code)\n\nType 'exit' To Exit \nSo Have Fun :) (@athede-v)")
 
-    def set_password(self):
-        is_valid_password = False
-        print("$4R >> Enter Your 8-Char Password (Type 'exit' to Cancel): ")
-        while not is_valid_password:
+#Varibale Managment
+password = ''
+isIn = False
+
+#Function Managment
+#    you can add some function for security
+#    like hash code or shuffle the password 
+#    MFA - Rate Limiting - avoid monitoring
+#    i don't know just do it
+
+while(True):
+    inpt = input("$UR >>> ")
+    if inpt == "exit" :
+        break
+    elif inpt == "set pass":
+        perm = False
+        print("$4R >> Enter Your 8-Char Password (Type 'exit' for Cancel) : ")
+        while(True):
             temp = input("$UR >>> ")
-            if len(temp) == 8:
-                is_valid_password = True
-                self.password = temp
-                print("$4R >> Password Set Successfully\n")
-            elif temp.lower() == 'exit':
+            if (len(temp) == 8) :
+                perm = True
+                break
+            elif (temp == 'x'):
                 break
             else:
-                print("$4R >> WRONG! Please enter an 8-character password or type 'exit' to cancel.")
-
-    def run_program(self):
-        if not self.is_in:
-            if self.password:
-                while True:
-                    random_indexes = [randint(1, 8) for _ in range(4)]
-                    password_com = ''.join(self.password[i - 1] for i in random_indexes)
+                print("$4R >> WRONG ! 8-Char Password (Type 'exit' for Cancel) : ")
+        if perm :
+            password = temp
+            print("$4R >> Password Setted\n")
+    elif inpt == "run":
+        if isIn == False :
+            if password != '' :
+                while True :
+                    random_indexes = [randint(1,8),randint(1,8),randint(1,8),randint(1,8)]
+                    password_com = password[random_indexes[0]-1]+password[random_indexes[1]-1]+password[random_indexes[2]-1]+password[random_indexes[3]-1]
                     print("\n\n    X-X-X-X")
-                    print(f"    {random_indexes[0]}-{random_indexes[1]}-{random_indexes[2]}-{random_indexes[3]}\n\n")
-
-                    temp_com = ''.join(input(f"    X[{i}] : ") for i in random_indexes)
-
-                    if temp_com == password_com:
-                        print("\n$4R >> Good Job, You're In\n")
-                        self.is_in = True
-                        break
-                    elif temp_com.lower() == 'exit':
+                    print("    ",random_indexes[0],'-',random_indexes[1],'-',random_indexes[2],'-',random_indexes[3],"\n\n",sep='')
+                    temp_com = ''
+                    temp_com += input("    X["+str(random_indexes[0])+"] : ")
+                    temp_com += input("    X["+str(random_indexes[1])+"] : ")
+                    temp_com += input("    X["+str(random_indexes[2])+"] : ")
+                    temp_com += input("    X["+str(random_indexes[3])+"] : ")
+                    if temp_com == password_com :
+                        print("\n$4R >> Good Job , You're In\n")
+                        isIn=True
                         break
                     else:
-                        print("\n$4R >> WRONG! Try Again")
+                        print("\n$4R >> WRONG ! Try Again")
             else:
                 print("$4R >> Please Set Password First (Type 'set pass')\n")
         else:
-            print("$4R >> You're Already In!\n")
-
-    def main_loop(self):
-        self.clear_screen()
-        print("Hello And Welcome To 4R Sign In Way :)\nThis Is The Way How It Works...")  # Add more information here if needed
-
-        # Main Loop
-        while True:
-            user_input = input("$UR >>> ")
-            if user_input.lower() == "exit":
-                break
-            elif user_input.lower() == "set pass":
-                self.set_password()
-            elif user_input.lower() == "run":
-                self.run_program()
-            else:
-                print("$4R >> Unknown Command\n")
-
-if __name__ == "__main__":
-    four_r_sign_in = FourRSignIn()
-    four_r_sign_in.main_loop()
+            print("$4R >> You're Already In .!\n")
+    else : 
+        print("$4R >> Unknown Command\n")
